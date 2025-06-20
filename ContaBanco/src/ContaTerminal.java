79e07598e266f022d58d9f64b70a3183bcce2b1a
VictorMarinho197 <victormarinho.ads2@gmail.com>
2025-06-20 07:59:44 -0300

    Verificação final da classe ContaTerminal e upload para repositório remoto

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -1,4 +1,16 @@
-package PACKAGE_NAME;
-
+import java.util.Scanner;
 public class ContaTerminal {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Informe o seu nome:");
+        String nomeCliente = scan.nextLine();
+        System.out.println("Informe a sua agência, com o dígito:");
+        String agencia = scan.nextLine();
+        System.out.println("Informe o número da conta:");
+        int numeroConta = scan.nextInt();
+        System.out.println("Agora, informe o seu saldo:");
+        double saldo = scan.nextDouble();
+
+        System.out.printf("Olá, %s, obrigado por criar uma conta em nosso banco, sua agência é %s, conta %d e seu saldo %.2f já está disponível para saque.", nomeCliente, agencia, numeroConta, saldo);
+    }
 }
